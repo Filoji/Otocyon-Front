c77196ad432948a997edfe5b05a0beaf21ba2dac
Filoji <michelvespier@gmail.com>
2024-03-24 11:35:46 +0100

    Basic Vue app + Axios + Gravatar component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import { ref } from "vue";
+
+const corps = ref("")
+axios
+  .get("http://localhost:3000/")
+  .then(
+    (response: AxiosResponse) => {corps.value = response.data["message"]}
+  )
+
+</script>
+
+<template>
+  <h1>Hi!</h1>
+  <p>{{ corps }}</p>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Gravatar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+export interface Props {
+  mail: string
+  size?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 200
+})
+
+async function hash(string: string) {
+  const utf8 = new TextEncoder().encode(string);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((bytes) => bytes.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
+
+const url = ref("");
+
+hash(props.mail)
+.then(
+  (hex: string) => url.value = `https://gravatar.com/avatar/${hex}"?d=identicon&s=${props.size}`
+);
+</script>
+
+<template>
+  <img :src="url" />
+</template>
